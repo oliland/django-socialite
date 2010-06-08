30ffbda9f8a3b1a15b9802cb87aecbbbade03ccd
Oli Kingshott <oli@oliland.net>
2010-06-08 12:35:07 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+
+
+setup(
+    name = "django-socialite",
+    version = "0.1",
+    author = "oliland",
+    author_email = "oli@oliland.net",
+    description = "Easy sign-ins with Facebook, LinkedIn and Twitter based on oauth-access",
+    long_description = open("README.md").read(),
+    license = "BSD",
+    url = "http://github.com/oliland/django-socialite",
+    packages = [
+        "socialite",
+        "socialite.facebookuser",
+        "socialite.linkedinuser",
+        "socialite.twitteruser",
+        "socialite.utils",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
